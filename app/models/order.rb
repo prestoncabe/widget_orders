b4f51c3606e60e1ed8d8d72783cee23646970932
Preston Cabe <cabep@ldc.upenn.edu>
2014-05-23 15:01:36 -0400

    Order validates widget_type

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,4 @@
 class Order < ActiveRecord::Base
-  validates :widget_type, presence: true
 
   validates_numericality_of :quantity, greater_than: 0, only_integer: true
 
@@ -8,5 +7,8 @@ class Order < ActiveRecord::Base
 
   validates_inclusion_of :date_needed_by,
     :in => (Date.today + 7)..Date.parse('2099-12-31') # TODO: is there a better way to do this?
+
+  validates_inclusion_of :widget_type,
+    :in => ['Widget', 'Widget Pro', 'Widget Xtreme']
 
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -5,4 +5,9 @@ describe Order do
     should ensure_inclusion_of(:color).
     in_array %w(red blue yellow)
   }
+
+  it {
+    should ensure_inclusion_of(:widget_type).
+    in_array ['Widget', 'Widget Pro', 'Widget Xtreme']
+  }
 end
